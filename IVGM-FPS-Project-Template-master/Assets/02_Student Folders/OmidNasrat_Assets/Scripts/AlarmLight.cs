dfde7e7a98bec952382bfdfdde929f797f10efe7
omedtjeee <omed_nasrat@hotmail.com>
2020-10-01 19:22:09 +0200

    lava spikes and light
    Added some beautiful lava and spikes to the map and a light fading in and out(alarming). No physics to the lava and spikes though. So you wont get damage from it at the moment

---
+++ IVGM-FPS-Project-Template-master/Assets/02_Student Folders/OmidNasrat_Assets/Scripts/AlarmLight.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmLight : MonoBehaviour
+{
+    private Light myLight;
+    public float maxIntensity = 50f;
+    public float minIntensity = 0f;
+    public float pulseSpeed = 25f;
+    private float targetIntensity = 50f;
+    private float currentIntensity;
+
+
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+    void Update()
+    {
+        currentIntensity = Mathf.MoveTowards(myLight.intensity, targetIntensity, Time.deltaTime * pulseSpeed);
+        if (currentIntensity >= maxIntensity) // grow towards the max intensity
+        {
+            currentIntensity = maxIntensity;
+            targetIntensity = minIntensity;
+        }
+        else if (currentIntensity <= minIntensity) // grow towards the min intensity
+        {
+            currentIntensity = minIntensity;
+            targetIntensity = maxIntensity;
+        }
+        myLight.intensity = currentIntensity; //update light intensity
+    }
+}
