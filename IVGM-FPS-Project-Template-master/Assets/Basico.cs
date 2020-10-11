e66226d905adc5c329df09b20779bc63096ba574
VitorDeSenna <vitordesenna@gmail.com>
2020-10-11 17:19:49 +0200

    Tiles work
    A few tiles are working. I sitll don't have the object (probably a lantern) and how the maze will work

---
+++ IVGM-FPS-Project-Template-master/Assets/Basico.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basico : MonoBehaviour
+{
+    public Transform Jogador;
+    
+
+
+    
+    void OnTriggerEnter(Collider theCollider)
+    {
+        if (theCollider.CompareTag("Player"))
+        {
+            GetComponent<Renderer>().material.color = Color.grey;
+        }
+
+
+    }
+
+  
+}
---
+++ IVGM-FPS-Project-Template-master/Assets/Chao_Cai.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chao_Cai : MonoBehaviour
+{
+
+
+
+    public GameObject Floor;
+    public Transform Jogador;
+
+
+    void OnTriggerEnter(Collider theCollider)
+    {
+        if (theCollider.CompareTag("Player")) //If player on top of the tile
+        {
+            //Floor.GetComponent<Renderer>().material.color = Color.red;
+            Floor.SetActive(false);
+            //Floor.GetComponent<Animation>().CrossFade("Falling");
+             
+        }
+    }
+   
+}
