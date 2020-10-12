3b1e86f92ae37297baf798a1216919429cce5bbd
VitorDeSenna <vitordesenna@gmail.com>
2020-10-12 21:42:29 +0200

    Plataforma fdp
    Plataform still doesn't work

--- IVGM-FPS-Project-Template-master/Assets/Falling.cs
+++ IVGM-FPS-Project-Template-master/Assets/Falling.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class Falling : MonoBehaviour
 {
+    bool isFalling = false;
+    float downSpeed = 0;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == "player")
+        {
+            isFalling = true;
+            Destroy(gameObject, 10);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,12 @@ public class Falling : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isFalling)
+        {
+            downSpeed += Time.deltaTime/10;
+            transform.position = new Vector3(transform.position.x,
+                transform.position.y-downSpeed,
+                transform.position.z);
+        }
     }
 }
