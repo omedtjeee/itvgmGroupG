78a64bd895f8f3625683c34a59b122116638adbc
J.J.J. Kooij <j.j.j.kooij@umail.leidenuniv.nl>
2020-10-14 14:57:35 +0200

    Start on scripting
    Created scripts to handle the enabling and disabling of platforms. Currently only works on mouse input

---
+++ IVGM-FPS-Project-Template-master/Assets/02_Student Folders/JasperKooij_Assets/Scripts/PlatformController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    private bool onoff;
+    // Start is called before the first frame update
+    void Start()
+    {
+        onoff = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            onoff = !onoff;
+            GetComponent<Renderer>().enabled = onoff;
+        }
+    }
+}
