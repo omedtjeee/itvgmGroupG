8c390217a879042b7807b9cb6bae246396b80e1b
J.J.J. Kooij <jasper.kooij2@gmail.com>
2020-10-14 17:27:14 +0200

    Scripting and Goggles
    Changing between colors is now possible.
    Added Goggles model (red only at the moment)
    Removed default weapon

--- IVGM-FPS-Project-Template-master/Assets/02_Student Folders/JasperKooij_Assets/Scripts/PlatformController.cs
+++ IVGM-FPS-Project-Template-master/Assets/02_Student Folders/JasperKooij_Assets/Scripts/PlatformController.cs
@@ -5,19 +5,61 @@ using UnityEngine;
 public class PlatformController : MonoBehaviour
 {
     private bool onoff;
+    private int color = 0;
+
+    private enum Platform
+    {
+        White,
+        Green,
+        Blue,
+        Red
+    };
+
     // Start is called before the first frame update
     void Start()
     {
-        onoff = true;
+        GetComponent<Renderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetMouseButtonDown(1))
+        {
+            color = 0;
+            ShowPlatform(color);
+        }
         if(Input.GetMouseButtonDown(0))
         {
-            onoff = !onoff;
-            GetComponent<Renderer>().enabled = onoff;
+            color = color % 3 + 1;
+            ShowPlatform(color);
+        }
+    }
+
+    void ShowPlatform(int colorIndex)
+    {
+        Renderer r = GetComponent<Renderer>();
+        Collider c = GetComponent<Collider>();
+
+        switch(colorIndex)
+        {
+            case 0:
+                r.enabled = false;
+                c.enabled = false;
+                break;
+            default:
+                if (gameObject.CompareTag( ((Platform)colorIndex).ToString()) )
+                {
+                    r.enabled = true;
+                    c.enabled = true;
+                }
+                else
+                {
+                    r.enabled = false;
+                    c.enabled = false;
+                }
+                break;
         }
     }
 }
