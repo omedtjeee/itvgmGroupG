b6228d4c8d1bc54421bf4fa7565442da429364cf
J.J.J. Kooij <j.j.j.kooij@umail.leidenuniv.nl>
2020-10-17 14:31:38 +0200

    Implementation UI Goggle Selection
    Added script and images
    Using modified copy of GameManager

---
+++ IVGM-FPS-Project-Template-master/Assets/GoggleSelection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoggleSelection : MonoBehaviour
+{
+    GameObject Green;
+    GameObject Blue;
+    GameObject Red;
+
+    public Sprite ActiveSelect;
+    public Sprite InactiveSelect;
+
+    private void Start()
+    {
+        Green = gameObject.transform.GetChild(0).gameObject;
+        Blue = gameObject.transform.GetChild(1).gameObject;
+        Red = gameObject.transform.GetChild(2).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Green.transform.GetChild(1).gameObject.GetComponent<Image>().sprite = ActiveSelect;
+        }
+    }
+}
