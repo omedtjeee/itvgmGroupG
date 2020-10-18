c7b68fe6e17e78659f0cc8ab74c61c1dd04fa780
J.J.J. Kooij <jasper.kooij2@gmail.com>
2020-10-18 20:22:41 +0200

    Goggle pickup script start

---
+++ IVGM-FPS-Project-Template-master/Assets/02_Student Folders/JasperKooij_Assets/Scripts/GogglePickup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GogglePickup : MonoBehaviour
+{
+
+    Collider col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Destroy(gameObject);
+
+    }
+}
