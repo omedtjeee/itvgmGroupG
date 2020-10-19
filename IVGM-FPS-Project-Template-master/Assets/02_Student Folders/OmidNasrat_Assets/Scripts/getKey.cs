7604467ea7e747fcb0e6d1dcd13816c2e20fd751
omedtjeee <omed_nasrat@hotmail.com>
2020-10-19 21:59:43 +0200

    level as good as complete just adding some complementary things

---
+++ IVGM-FPS-Project-Template-master/Assets/02_Student Folders/OmidNasrat_Assets/Scripts/getKey.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getKey : MonoBehaviour
+{
+
+    public AutomticOpening automaticDoor;
+    Pickup m_Pickup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Pickup = GetComponent<Pickup>();
+        DebugUtility.HandleErrorIfNullGetComponent<Pickup,getKey>(m_Pickup,this,gameObject);
+
+        //subscribe to pickup action
+        m_Pickup.onPick += OnPicked;
+    }
+
+    void OnPicked(PlayerCharacterController other) {
+        if(automaticDoor.key1 == false) {
+            automaticDoor.key1 = true;
+            Destroy(gameObject);
+        }
+        else {
+            automaticDoor.key2 = true;
+            Destroy(gameObject);
+        }
+       
+
+        
+       
+    }
+}
