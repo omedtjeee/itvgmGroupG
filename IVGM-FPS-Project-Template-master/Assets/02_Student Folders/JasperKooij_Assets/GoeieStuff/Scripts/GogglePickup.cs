7bc81d83b1c8d12e032803bbfeecccbe73bbeeb6
Jasper <31314110+MrJasperge@users.noreply.github.com>
2020-10-20 21:45:01 +0200

    Add goeie files

---
+++ IVGM-FPS-Project-Template-master/Assets/02_Student Folders/JasperKooij_Assets/GoeieStuff/Scripts/GogglePickup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GogglePickup : MonoBehaviour
+{
+    public GoggleSelection gs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gs = GameObject.FindObjectOfType(typeof(GoggleSelection)) as GoggleSelection;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        PlayerCharacterController pickingPlayer = other.GetComponent<PlayerCharacterController>();
+
+        if (pickingPlayer != null)
+        {
+            gs.Collected();
+            
+            Destroy(gameObject);
+        }
+
+    }
+}
